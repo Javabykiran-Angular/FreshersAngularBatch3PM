8507fa933d117a63227a419323d95630d6678f50
Javabykiran-Angular <71135924+Javabykiran-Angular@users.noreply.github.com>
2021-02-15 16:04:15 +0530

    Day3

---
+++ Typescript/Day3/demo1.js
@@ -0,0 +1,53 @@
+//if else
+// if(12<4){
+//     console.log("Condition is true....");
+// }else{
+//     console.log("Condition is false....");
+// }
+var num = 2;
+// if(num<4){
+//     console.log("Condition is true....");
+//     if(num==2){
+//         console.log("Value is "+num);
+//     }
+// }
+// else{
+//     console.log("Condition is false....");
+// }
+//switch
+var choice = 12;
+// switch(choice){
+//     case 1:console.log('Case 1 is execute');
+//             break;
+//     case 2: console.log('Case 2 is execute');
+//             break;
+//     default : console.log('Default Case is execute');
+// }
+//while
+var num1 = 5;
+// while(num1!=0){
+//     console.log("Value of num1 is "+num1);
+//     num1--;
+// }
+//do -while
+// do{
+//     console.log("Value of num1 is "+num1);
+//     num1--;
+// }while(num1!=0);
+for (var i = 0; i < 4; i++) {
+    //   console.log('Value of I is '+i);
+}
+// console.log("I value is "+i); 
+// var => it has global scope
+// let => it scope is nearest within that block
+for (var j = 0; j < 4; j++) {
+    // console.log('Value of J is '+j);
+}
+//  console.log("---------- J is "+j);
+//const => it is used as vaiable but once u craete 
+//variable as a const with value then you can not change 
+//that value within program. It has global scope
+//final 
+var pi = 3.14;
+console.log("Constant value is " + pi);
+// var, let , const
---
+++ Typescript/Day3/demo2.js
@@ -0,0 +1,41 @@
+// 1 function without argument & without return type 
+// void display(){
+// system.out.print();
+// }
+// 2 function with argument & without return type
+// void setId(int a){
+// this.id=a;
+// }
+// 3 function without argument & with return type
+// int getId(){
+//     return (this.id);
+// }
+// 4 function with argument & with return type
+// int area(int b){
+//     res=b*b;
+//     return res;
+// }
+// add(); 
+//Implement First type 
+//function without argument & without return type
+function add() {
+    console.log("Addition is " + (2 + 3));
+}
+// add(); //function call
+// 2 function with argument & without return type
+function add1(a, b) {
+    console.log("Addition is " + (a + b));
+}
+// add1(5,5);
+// 3 function without argument & with return type
+function add2() {
+    return (6 + 6);
+}
+// var res=add2();
+// console.log("Result is "+res);
+// console.log("Result is===> "+add2());
+// 4 function with argument & with return type
+function add4(a, b) {
+    return (a + b);
+}
+console.log("Addition is " + add4(8, 8));
